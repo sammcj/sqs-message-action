e9c0094cd710e477da6ec10b31080b797398fed6
Sam McLeod <sammcj@users.noreply.github.com>
2022-07-25 11:21:26 +1000

    Working version v1.0.0

---
+++ index.js
@@ -0,0 +1,41 @@
+const core = require('@actions/core');
+const aws = require('aws-sdk');
+
+async function run() {
+  try {
+    const sqsUrl = core.getInput('sqs-url', { required: true });
+    const message = core.getInput('message', { required: true });
+    const messageGroupId = core.getInput('message-group-id', { required: false });
+    const messageAttributes = core.getInput('message-attributes', { required: false });
+
+    const params = {
+      QueueUrl: sqsUrl,
+      MessageBody: message,
+      MessageGroupId: messageGroupId,
+    }
+
+    console.log(`value of messageAttributes: ${messageAttributes}`);
+
+    if (messageAttributes) {
+      params.MessageAttributes = JSON.parse(messageAttributes);
+    }
+
+    const sqs = new aws.SQS();
+    sqs.sendMessage(params, (err, resp) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(`resp ${JSON.stringify(resp, null, 2)}`);
+      }
+    })
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+  run();
+}
