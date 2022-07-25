0d991de49c0ef1e15f1507525907218c4ac9e6ef
Sam McLeod <sammcj@users.noreply.github.com>
2022-07-25 11:21:26 +1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const core = require('@actions/core');
+const aws = require('aws-sdk');
+
+async function run() {
+  try {
+    const sqsUrl = core.getInput('sqs-url', { required: true });
+    const message = core.getInput('message', { required: true });
+    const params = {
+      QueueUrl: sqsUrl,
+      MessageBody: message,
+    };
+
+    const sqs = new aws.SQS();
+    sqs.sendMessage(params, (err, resp) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(`resp ${JSON.stringify(resp, null, 2)}`);
+      }
+    })
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+module.exports = run;
+
+/* istanbul ignore next */
+if (require.main === module) {
+  run();
+}
