14ae72281aa3ce178ca4d9839574e08a36c6e62d
Sam McLeod <sammcj@users.noreply.github.com>
2022-07-25 14:27:19 +1000

    Initial commit

---
+++ node_modules/eslint-plugin-deprecation/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.rules = void 0;
+const rules = require("./rules");
+exports.rules = rules;
+//# sourceMappingURL=index.js.map
