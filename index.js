d0c1a858ed4b267ea51ec48bf7e1637e072b7462
Sam McLeod <sammcj@users.noreply.github.com>
2022-10-04 09:16:08 +1100

    chore: update workflows

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const core = require('@actions/core')
-const aws = require('aws-sdk')
+const core = require('@actions/core');
+const aws = require('aws-sdk');
 
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/SQS.html
 
 async function run() {
   try {
-    const sqsUrl = core.getInput('sqs-url', { required: true })
-    const message = core.getInput('message', { required: true })
-    const messageGroupId = core.getInput('message-group-id', { required: false })
-    const messageAttributes = core.getInput('message-attributes', { required: false })
+    const sqsUrl = core.getInput('sqs-url', { required: true });
+    const message = core.getInput('message', { required: true });
+    const messageGroupId = core.getInput('message-group-id', { required: false });
+    const messageAttributes = core.getInput('message-attributes', { required: false });
 
     if (message === '') {
-      throw new Error('Message cannot be an empty string')
+      throw new Error('Message cannot be an empty string');
     }
 
     // If any of the required inputs are missing or an empty string, throw an error
     if (sqsUrl === '' || message === '') {
-      console.log(`sqsUrl: ${sqsUrl}, message: ${message}`)
-      throw new Error('You must provide at least a valid SQS URL and a message')
+      console.log(`sqsUrl: ${sqsUrl}, message: ${message}`);
+      throw new Error('You must provide at least a valid SQS URL and a message');
     }
 
     // Construct the parameters
@@ -25,32 +25,32 @@ async function run() {
       QueueUrl: sqsUrl,
       MessageBody: message,
       MessageGroupId: messageGroupId,
-    }
+    };
 
     // If message attributes are provided, add them to the params object
     if (messageAttributes) {
-      params.MessageAttributes = JSON.parse(messageAttributes)
+      params.MessageAttributes = JSON.parse(messageAttributes);
     }
 
     // Create a new SQS service object
-    const sqs = new aws.SQS()
+    const sqs = new aws.SQS();
 
     // Send the message to the queue and log the response
     sqs.sendMessage(params, (err, resp) => {
       if (err) {
-        throw err
+        throw err;
       } else {
-        console.log(`resp ${JSON.stringify(resp, null, 2)}`)
+        console.log(`resp ${JSON.stringify(resp, null, 2)}`);
       }
-    })
+    });
   } catch (error) {
-    core.setFailed(error.message)
+    core.setFailed(error.message);
   }
 }
 
-module.exports = run
+module.exports = run;
 
 /* istanbul ignore next */
 if (require.main === module) {
-  run()
+  run();
 }
